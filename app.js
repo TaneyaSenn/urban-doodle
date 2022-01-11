a82659e3f51ef378f4583615b37c3a96e63c85dd
Taneya <twil609@aucklanduni.ac.nz>
2022-01-11 11:59:15 +1000

    2

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/cat", function(req, res){
+    const cat = {
+        ears: 2,
+        sound: "meow",
+        name: "Fluffy"
+    }
+    res.json(cat);
+});
+
+
+app.listen(port, function(){
+    console.log(`App listening on port ${port}!`);
+});
